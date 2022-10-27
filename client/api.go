144bfa7eb0d5357113ae8138f7c2f9be317ad4df
Vid Drobnic <vid.drobnic@gmail.com>
2022-10-27 12:41:57 +0200

    add tax to api

--- client/api.go
+++ client/api.go
@@ -4,12 +4,14 @@ import (
 	"github.com/zerodays/woocommerce-go/cart"
 	"github.com/zerodays/woocommerce-go/internal/backend"
 	"github.com/zerodays/woocommerce-go/order"
+	"github.com/zerodays/woocommerce-go/tax"
 )
 
 // API is the API client. It should be created with the New function.
 type API struct {
 	Order *order.Client
 	Cart  *cart.Client
+	Tax   *tax.Client
 }
 
 // Init initializes the API client with given credentials.
@@ -21,6 +23,7 @@ func (a *API) Init(baseURL, consumerKey, consumerSecret string) {
 
 	a.Order = order.New(b)
 	a.Cart = cart.New(b)
+	a.Tax = tax.New(b)
 }
 
 // New creates a new API client with given credentials.

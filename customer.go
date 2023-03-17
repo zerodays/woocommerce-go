d9f5523e9f0ec7fb8490bb0d43f83e6bbb6deae6
Vid Drobnic <vid@zerodays.dev>
2023-03-17 08:24:06 +0100

    add omitempty to customer serialization

--- customer.go
+++ customer.go
@@ -1,17 +1,17 @@
 package woocommerce
 
 type Customer struct {
-	ID               int        `json:"id"`
-	DateCreated      string     `json:"date_created"`
-	DateModified     string     `json:"date_modified"`
-	Email            string     `json:"email"`
-	FirstName        string     `json:"first_name"`
-	LastName         string     `json:"last_name"`
-	Role             string     `json:"role"`
-	Username         string     `json:"username"`
-	Billing          Address    `json:"billing"`
-	Shipping         Address    `json:"shipping"`
-	IsPayingCustomer bool       `json:"is_paying_customer"`
-	AvatarUrl        string     `json:"avatar_url"`
-	MetaData         []MetaData `json:"meta_data"`
+	ID               int        `json:"id,omitempty"`
+	DateCreated      string     `json:"date_created,omitempty"`
+	DateModified     string     `json:"date_modified,omitempty"`
+	Email            string     `json:"email,omitempty"`
+	FirstName        string     `json:"first_name,omitempty"`
+	LastName         string     `json:"last_name,omitempty"`
+	Role             string     `json:"role,omitempty"`
+	Username         string     `json:"username,omitempty"`
+	Billing          Address    `json:"billing,omitempty"`
+	Shipping         Address    `json:"shipping,omitempty"`
+	IsPayingCustomer bool       `json:"is_paying_customer,omitempty"`
+	AvatarUrl        string     `json:"avatar_url,omitempty"`
+	MetaData         []MetaData `json:"meta_data,omitempty"`
 }

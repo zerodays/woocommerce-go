07283c5e9dbe12fb13cf5a71102fd4d585824cbb
Vid Drobnic <vid@zerodays.dev>
2023-05-23 13:59:53 +0200

    add extra fields to error

--- error.go
+++ error.go
@@ -1,12 +1,19 @@
 package woocommerce
 
+type ErrorDetails struct {
+	Code             string         `json:"code"`
+	Message          string         `json:"message"`
+	AdditionalErrors []ErrorDetails `json:"additional_errors"`
+}
+
 type Error struct {
 	StatusCode int    `json:"status_code"`
 	Code       string `json:"code"`
 	Message    string `json:"message"`
 	Data       struct {
-		Status int               `json:"status"`
-		Params map[string]string `json:"params"`
+		Status  int                     `json:"status"`
+		Params  map[string]string       `json:"params"`
+		Details map[string]ErrorDetails `json:"details"`
 	}
 }
 
